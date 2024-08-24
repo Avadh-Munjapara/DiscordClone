12a064bc8ba9c16bf94217da1f1481cc34f53856
Avadh11 <155130440+Avadh-Munjapara@users.noreply.github.com>
2024-08-24 12:05:42 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      maxWidth:{
+      mw:"1200px",
+    },
+    fontFamily:{
+      Whitney:["Whitney"],
+      Ginto:["Ginto"],
+      ggSans:["ggSans"],
+    },
+   },
+  },
+  plugins: [],
+  
+}
+
